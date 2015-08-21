cf9470c3ca71aa3ffcfb1ddfe0457014a0a9ccf9
Austin English <austin@gaikai.com>
2015-08-21 12:34:41 -0500

    valgrind-full.sh: bug 36163 is fixed upstream

--- valgrind/valgrind-full.sh
+++ valgrind/valgrind-full.sh
@@ -214,7 +214,6 @@ then
     touch dlls/winhttp/tests/winhttp.ok # https://bugs.winehq.org/show_bug.cgi?id=36599 winhttp.c:879: Test failed: failed to receive response 12152
     touch dlls/wininet/tests/http.ok # https://bugs.winehq.org/show_bug.cgi?id=36637 
     touch dlls/ws2_32/tests/sock.ok # https://bugs.winehq.org/show_bug.cgi?id=36681 sock.c:2270: Test failed: Expected 10047, received 10043
-    touch programs/cmd/tests/batch.ok # https://bugs.winehq.org/show_bug.cgi?id=36163
     touch programs/xcopy/tests/xcopy.ok # https://bugs.winehq.org/show_bug.cgi?id=36172
 fi
 
