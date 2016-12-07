d467970abf2745642c7b828be6f5a01d58783614
Austin English <austinenglish@gmail.com>
2016-12-07 05:05:18 -0600

    valgrind-full.sh: be more fine grained with skipping tests for driver bugs
    Instead of disabling globally, only disable on affected systems

--- valgrind/valgrind-full.sh
+++ valgrind/valgrind-full.sh
@@ -156,6 +156,7 @@ $WINE start /min winemine
 make testclean
 
 # valgrind bugs:
+# FIXME: ddraw stuff should be checked, may be driver bugs?
 touch dlls/ddraw/tests/ddraw1.ok # https://bugs.kde.org/show_bug.cgi?id=264785
 touch dlls/ddraw/tests/ddraw2.ok # https://bugs.kde.org/show_bug.cgi?id=264785
 touch dlls/ddraw/tests/ddraw4.ok # valgrind assertion failure https://bugs.winehq.org/show_bug.cgi?id=36261
@@ -165,6 +166,92 @@ touch dlls/kernel32/tests/thread.ok # valgrind crash https://bugs.winehq.org/sho
 touch dlls/kernel32/tests/virtual.ok # valgrind assertion failure after https://bugs.winehq.org/show_bug.cgi?id=28816: valgrind: m_debuginfo/debuginfo.c:1261 (vgPlain_di_notify_pdb_debuginfo): Assertion 'di && !di->fsm.have_rx_map && !di->fsm.have_rw_map' failed.
 touch dlls/msvcrt/tests/string.ok # valgrind wontfix: https://bugs.winehq.org/show_bug.cgi?id=36165
 
+# graphics driver bugs;
+
+    # http://pclinuxoshelp.com/index.php/Glxinfo is helpful for some example glxinfo output
+    # FIXME: split out graphics failures by video vendor (amd/intel/nvidia), and detect card using glxinfo
+    # not perfect, but that gets some better coverage
+    # May also want to distinguish between propritary and open source, as well for amd/nvidia
+
+    # Sample info:
+    # amd (flgrx): ATI Technologies Inc.
+    # amd (foss): X.Org
+    # intel: Intel Open Source Technology Center
+    # nvidia (nouveau): nouveau
+    # nvidia (nvidia): NVIDIA Corporation
+    card_string="$(glxinfo 2>&1 | grep 'OpenGL vendor string')"
+    case "${card_string}" in
+        *AMD*|*ATI*|*amd*|*ati*|*X.Org*) card=amd ;;
+        *INTEL*|*Intel*|*intel*) card=intel ;;
+        *NOUVEAU*|*Nouveau*|*nouveau*|*NVIDIA*|*Nvidia*|*nvidia*) card=nvidia ;;
+        *) card=unknown ;;
+    esac
+
+    # Sample info:
+    # amd (flgrx) 4.5.13411 Compatibility Profile Context FireGL 15.201.2401
+    # amd (foss): 3.0 Mesa 12.0.1
+    # intel: 3.0 Mesa 9.1.3
+    # nvidia (nouveau): 3.0 Mesa 9.2.5
+    # nvidia (nvidia): 3.3.0 NVIDIA 319.32
+    driver_string="$(glxinfo 2>&1 | grep 'OpenGL version string')"
+    case "${card_string}" in
+        *FIREGL|*FireGL*|*firegl*) driver=amd ;;
+        *MESA*|*Mesa*|*mesa*) driver=mesa ;;
+        *NVIDIA*|*Nvidia*|*nvidia*) driver=nvidia ;;
+        *) driver=unknown ;;
+    esac
+
+    echo "Detected graphic card info:"
+    echo "Video card: $card_string"
+    echo "Video driver: $driver_string"
+    echo "Video card vendor: $card"
+    echo "Video driver vendor: $driver"
+
+    if echo "$card $driver" | grep -q "unknown" ; then
+        echo "Your video card could not be detected. Please file a bug and attach \`glxinfo\` output."
+    fi
+
+    # FIXME: possible driver bug groups
+    # will need to comment out all graphics tests below, run make test,
+    # then start fleshing out failures under groups below
+    # Note: Need to teest what crashes on all available hardware
+
+    # fglrx driver bugs
+    # mesa driver bugs (amd/intel/nouveau, two or more are affected)
+    # nouveau driver bugs
+    # nvidia driver bugs
+
+    # intel hardware bugs (check amd/nouveau first to see if it's a mesa bug)
+    # amd hardware bugs (affects both fglrx and mesa)
+    # nvidia hardware bugs (affects both nouveau and nvidia)
+
+##
+ # FIXME: sort me out:
+
+    # Intel/Nvidia (AMD mesa?)
+    #touch dlls/d3dx9_36/tests/mesh.ok # test fails on intel/nvidia https://bugs.winehq.org/show_bug.cgi?id=28810
+    # FIXME: for video driver bugs, group by driver:
+    # FIXME: maybe get super fancy and list by vendor and driver
+    # e.g., amd: fglrx/radeon (mesa), nvidia: nvidia/nouveau (mesa), intel: mesa
+
+    # Nvidia driver bugs, and I'm not currently running any nvidia hardware
+    # touch dlls/d2d1/tests/d2d1.ok # crashes https://bugs.winehq.org/show_bug.cgi?id=38481
+    # touch dlls/ddraw/tests/d3d.ok # test crashes on nvidia https://bugs.winehq.org/show_bug.cgi?id=36660
+    # touch dlls/ddrawex/tests/surface.ok # valgrind segfaults on nvidia https://bugs.winehq.org/show_bug.cgi?id=36689 / https://bugs.kde.org/show_bug.cgi?id=335907
+
+    #touch dlls/d3d8/tests/visual.ok # https://bugs.winehq.org/show_bug.cgi?id=35862 visual.c:13837: Test failed: Expected color 0x000000ff, 0x000000ff, 0x00ff00ff or 0x00ff7f00 for instruction "rcp1", got 0x00ff0000 (nvidia)
+    #touch dlls/d3d9/tests/device.ok # https://bugs.kde.org/show_bug.cgi?id=335563 upstream comment: "I don't think there's anything that can be done here without considerable effort."
+    #touch dlls/d3d9/tests/visual.ok # https://bugs.winehq.org/show_bug.cgi?id=35862 visual.c:13837: Test failed: Expected color 0x000000ff, 0x000000ff, 0x00ff00ff or 0x00ff7f00 for instruction "rcp1", got 0x00ff0000.
+
+    # Still present?
+    #touch dlls/d3d8/tests/device.ok # test crashes https://bugs.winehq.org/show_bug.cgi?id=28800
+
+    # crashes in radeonsi_dri.so..init opengl info? Is display not right over ssh, is valgrind screwy, wine change?
+    # presumably anything gui would be affected
+    #  amstream/amstream.ok
+########
+
+# FIXME: audit bugs:
 # hanging bugs:
 touch dlls/ieframe/tests/ie.ok # FIXME: hangs
 touch dlls/mshtml/tests/events.ok # https://bugs.winehq.org/show_bug.cgi?id=37157 hangs under valgrind
@@ -175,24 +262,17 @@ touch dlls/ole32/tests/marshal.ok # FIXME: hangs
 touch dlls/user32/tests/win.ok # https://bugzilla.redhat.com/show_bug.cgi?id=1248314
 
 # wine bugs:
-touch dlls/winmm/tests/mci.ok # https://bugs.winehq.org/show_bug.cgi?id=30557
+#touch dlls/winmm/tests/mci.ok # https://bugs.winehq.org/show_bug.cgi?id=30557
 
-# wine hangs with pdb debug builds in dlls/ieframe/tests/ie.c (https://bugs.winehq.org/show_bug.cgi?id=38604)
-# and (sometimes) in dlls/atl100/tests/atl.c (https://bugs.winehq.org/show_bug.cgi?id=38594)
 if [ $gecko_pdb -eq 1 ]
 then
-    touch dlls/atl100/tests/atl.ok
-    touch dlls/ieframe/tests/ie.ok
+    touch dlls/atl100/tests/atl.ok # wine (sometimes) hangs with pdb debug builds in dlls/atl100/tests/atl.c (https://bugs.winehq.org/show_bug.cgi?id=38594)
+    touch dlls/ieframe/tests/ie.ok # wine hangs with pdb debug builds in dlls/ieframe/tests/ie.c (https://bugs.winehq.org/show_bug.cgi?id=38604)
 fi
 
 if [ $skip_crashes -eq 1 ]
 then
     touch dlls/crypt32/tests/msg.ok # crashes https://bugs.winehq.org/show_bug.cgi?id=36200
-    touch dlls/d2d1/tests/d2d1.ok # crashes https://bugs.winehq.org/show_bug.cgi?id=38481
-    touch dlls/d3d8/tests/device.ok # test crashes https://bugs.winehq.org/show_bug.cgi?id=28800
-    touch dlls/d3dx9_36/tests/mesh.ok # test fails on intel/nvidia https://bugs.winehq.org/show_bug.cgi?id=28810
-    touch dlls/ddraw/tests/d3d.ok # test crashes on nvidia https://bugs.winehq.org/show_bug.cgi?id=36660
-    touch dlls/ddrawex/tests/surface.ok # valgrind segfaults on nvidia https://bugs.winehq.org/show_bug.cgi?id=36689 / https://bugs.kde.org/show_bug.cgi?id=335907
     touch dlls/kernel32/tests/debugger.ok # intentional
     touch dlls/ntdll/tests/exception.ok # https://bugs.winehq.org/show_bug.cgi?id=28735
     touch dlls/user32/tests/dde.ok # https://bugs.winehq.org/show_bug.cgi?id=39257
@@ -206,10 +286,7 @@ then
         touch dlls/comctl32/tests/propsheet.ok # https://bugs.winehq.org/show_bug.cgi?id=36238
         touch dlls/user32/tests/win.ok # https://bugs.winehq.org/show_bug.cgi?id=36682 win.c:2244: Test succeeded inside todo block: GetActiveWindow() = 0x1200c4
     fi
-    
-    touch dlls/d3d8/tests/visual.ok # https://bugs.winehq.org/show_bug.cgi?id=35862 visual.c:13837: Test failed: Expected color 0x000000ff, 0x000000ff, 0x00ff00ff or 0x00ff7f00 for instruction "rcp1", got 0x00ff0000 (nvidia)
-    touch dlls/d3d9/tests/device.ok # https://bugs.kde.org/show_bug.cgi?id=335563 device.c:3587: Test failed: cw is 0xf7f, expected 0xf60.
-    touch dlls/d3d9/tests/visual.ok # https://bugs.winehq.org/show_bug.cgi?id=35862 visual.c:13837: Test failed: Expected color 0x000000ff, 0x000000ff, 0x00ff00ff or 0x00ff7f00 for instruction "rcp1", got 0x00ff0000.
+
     touch dlls/gdi32/tests/font.ok # https://bugs.winehq.org/show_bug.cgi?id=36234 font.c:3607: Test succeeded inside todo block: W: tmFirstChar for Mathematica1 got 00 expected 00
     touch dlls/gdiplus/tests/font.ok # https://bugs.winehq.org/show_bug.cgi?id=28097 font.c:784: Test failed: wrong face name Liberation Sans
     touch dlls/kernel32/tests/heap.ok # https://bugs.winehq.org/show_bug.cgi?id=36673 heap.c:1138: Test failed: 0x10: got heap flags 00000002 expected 00000020
